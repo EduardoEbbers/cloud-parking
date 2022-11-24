9cceedbd13265e69a33badb7e42a3fffe63517e6
Carlos Ebbers <cel.ebbers@gmail.com>
2022-11-24 18:28:11 -0300

    findAll e testes

--- src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -1,8 +1,10 @@
 package one.digitalinnovation.parking.service;
 
+import one.digitalinnovation.parking.controller.ParkingNotFoundException;
 import one.digitalinnovation.parking.model.Parking;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,17 +15,49 @@ import java.util.stream.Collectors;
 public class ParkingService {
     private static Map<String, Parking> parkingMap = new HashMap<>();
 
-    static {
-        var id = getUUID();
-        Parking parking = new Parking(id, "DSS-1111", "SC", "CELTA", "PRETO");
-        parkingMap.put(id, parking);
-    }
-
     public List<Parking> findAll() {
         return parkingMap
                 .values()
                 .stream()
                 .collect(Collectors.toList());
+    }
+
+    public Parking findById(String id) {
+        Parking parking = parkingMap.get(id);
+        if(parking == null) {
+            throw new ParkingNotFoundException(id);
+        }
+        return parkingMap
+                .get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
+    }
+
+    public Parking update(String id, Parking parkingCreate) {
+        Parking parking = findById(id);
+        parking.setColor(parkingCreate.getColor());
+        parkingMap.replace(id, parking);
+        return parking;
+    }
+
+    public void delete(String id) {
+        findById(id);
+        parkingMap
+                .remove(id);
+    }
+
+    public Parking exit(String id) {
+        // recuperar o estacionamento
+        // atualizar data de saida
+        // calcular o valor
+
+        return null;
     }
 
     private static String getUUID() {
